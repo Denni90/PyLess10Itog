27a180f10979c08dce900d2c03b036ace3d1ac8c
denis.tsapenko <denis.tsapenko90@yandex.ru>
2023-12-19 18:56:52 +0300

    Комментарий

--- less10_2.py
+++ less10_2.py
@@ -1,19 +1,19 @@
-'''Задача 44: В ячейке ниже представлен код
+"""Задача 44: В ячейке ниже представлен код
 генерирующий DataFrame, которая состоит всего из 1 столбца.
 Ваша задача перевести его в one hot вид.
-Сможете ли вы это сделать без get_dummies?'''
+Сможете ли вы это сделать без get_dummies?"""
 
 import random
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 
-lst = ['robot'] * 10
-lst += ['human'] * 10
-random.shuffle(lst)
-data = pd.DataFrame({'whoAmI':lst})
+lst = ['robot'] * 10  # В этой строке создается список 10 строк "робот"
+lst += ['human'] * 10  # Добавляем список из 10 строк "человек"
+random.shuffle(lst)  # Перемешиваем списки с помощью функции shuffle и модуля random
+data = pd.DataFrame({'whoAmI': lst})  # Создаем список из перемешаных строк
 
-encoder = OneHotEncoder(sparse_output=False)
-encoder.fit_transform(data[['whoAmI']])
-data_head = encoder.fit_transform(data[['whoAmI']])
-data_head_df = pd.DataFrame(data_head, columns=encoder.categories_[0])
-print(data_head_df)
+encoder = OneHotEncoder(sparse_output=False)  # Создаем новый объект
+encoder.fit_transform(data[['whoAmI']])  # Создадим столбец
+data_head = encoder.fit_transform(data[['whoAmI']])  # Присвоим новые данные
+data_head_df = pd.DataFrame(data_head, columns=encoder.categories_[0])  # Создадим новые строки для таблицы
+print(data_head_df)  # Вывод данных
